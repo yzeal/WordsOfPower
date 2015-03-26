a5afb7a61ba2d52ab396694ac60e35fb658d722b
yzeal <yzeal@live.de>
2015-03-26 16:55:07 +0100

    Rotation sollte jetzt eventuell vielleicht ordentlich funktionieren. >.>

---
+++ Assets/Scripts/RotationPivot.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationPivot : MonoBehaviour {
+
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = player.transform.position + Vector3.up*0.722f;
+	}
+}
