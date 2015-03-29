335088b5f19d6ee2d56306edafa93860c5c3ff2f
yzeal <yzeal@live.de>
2015-03-29 21:44:21 +0200

    Tasten geben alle wieder Töne von sich

--- Assets/Scripts/PianoKeys.cs
+++ Assets/Scripts/PianoKeys.cs
@@ -28,89 +28,89 @@ public class PianoKeys : MonoBehaviour {
 	void Update () {
 		if(wop.typing && wopGUI.currentState == WoPGUIStates.HUD){
 
-			foreach(string keyString in keyboardKeys){
-				try{
-					if(Input.GetKeyDown(keyString)){
-						if(!keyboardToPiano[keyString].isPlaying){
-							keyboardToPiano[keyString].Play();
-						}
-					}
-					if(Input.GetKeyUp(keyString)){
-						keyboardToPiano[keyString].Stop();
-					}
-				}catch(System.Exception e){
-					//do nothing
-				}
-			}
+//			foreach(string keyString in keyboardKeys){
+//				try{
+//					if(Input.GetKeyDown(keyString)){
+//						if(!keyboardToPiano[keyString].isPlaying){
+//							keyboardToPiano[keyString].Play();
+//						}
+//					}
+//					if(Input.GetKeyUp(keyString)){
+//						keyboardToPiano[keyString].Stop();
+//					}
+//				}catch(System.Exception e){
+//					//do nothing
+//				}
+//			}
+//
+//
+//			if(Input.anyKeyDown){
+//				string pressedKey = Input.inputString;
+//				if(pressedKey.Contains("!")){
+//					if(!keyboardToPiano["!"].isPlaying){
+//						keyboardToPiano["!"].Play();
+//					}
+//				}else{
+//					keyboardToPiano["!"].Stop();
+//				}
+//				if(pressedKey.Contains("?")){
+//					if(!keyboardToPiano["?"].isPlaying){
+//						keyboardToPiano["?"].Play();
+//					}
+//				}else{
+//					keyboardToPiano["?"].Stop();
+//				}
+//				if(pressedKey.Contains("ä") || pressedKey.Contains("Ä")){
+//					if(!keyboardToPiano["ä"].isPlaying){
+//						keyboardToPiano["ä"].Play();
+//					}
+//				}else{
+//					keyboardToPiano["ä"].Stop();
+//				}
+//				if(pressedKey.Contains("ö") || pressedKey.Contains("Ö")){
+//					if(!keyboardToPiano["ö"].isPlaying){
+//						keyboardToPiano["ö"].Play();
+//					}
+//				}else{
+//					keyboardToPiano["ö"].Stop();
+//				}
+//				if(pressedKey.Contains("ü") || pressedKey.Contains("Ü")){
+//					if(!keyboardToPiano["ü"].isPlaying){
+//						keyboardToPiano["ü"].Play();
+//					}
+//				}else{
+//					keyboardToPiano["ü"].Stop();
+//				}
+//			}
+//
+//			if(!Input.anyKey){
+//				keyboardToPiano["ä"].Stop();
+//				keyboardToPiano["ö"].Stop();
+//				keyboardToPiano["ü"].Stop();
+//				keyboardToPiano["!"].Stop();
+//				keyboardToPiano["?"].Stop();
+//			}
 
-
-			if(Input.anyKeyDown){
-				string pressedKey = Input.inputString;
-				if(pressedKey.Contains("!")){
-					if(!keyboardToPiano["!"].isPlaying){
-						keyboardToPiano["!"].Play();
-					}
-				}else{
-					keyboardToPiano["!"].Stop();
-				}
-				if(pressedKey.Contains("?")){
-					if(!keyboardToPiano["?"].isPlaying){
-						keyboardToPiano["?"].Play();
-					}
-				}else{
-					keyboardToPiano["?"].Stop();
-				}
-				if(pressedKey.Contains("ä") || pressedKey.Contains("Ä")){
-					if(!keyboardToPiano["ä"].isPlaying){
-						keyboardToPiano["ä"].Play();
-					}
-				}else{
-					keyboardToPiano["ä"].Stop();
-				}
-				if(pressedKey.Contains("ö") || pressedKey.Contains("Ö")){
-					if(!keyboardToPiano["ö"].isPlaying){
-						keyboardToPiano["ö"].Play();
-					}
-				}else{
-					keyboardToPiano["ö"].Stop();
-				}
-				if(pressedKey.Contains("ü") || pressedKey.Contains("Ü")){
-					if(!keyboardToPiano["ü"].isPlaying){
-						keyboardToPiano["ü"].Play();
-					}
-				}else{
-					keyboardToPiano["ü"].Stop();
-				}
-			}
-
-			if(!Input.anyKey){
-				keyboardToPiano["ä"].Stop();
-				keyboardToPiano["ö"].Stop();
-				keyboardToPiano["ü"].Stop();
-				keyboardToPiano["!"].Stop();
-				keyboardToPiano["?"].Stop();
-			}
-
-			//			if(Input.anyKey){
-			//				string pressedKey = Input.inputString;
-			//				if(Input.GetKeyDown(".")){
-			//					keyboardToPiano["."].loop = true;
-			//					keyboardToPiano["."].Play();
-			//				}
-			//				if(Input.GetKeyUp(".")){
-			//					keyboardToPiano["."].Stop();
-			//				}
-			//				try{
-			//					AudioSource pianoKey = keyboardToPiano[pressedKey];
-			//					if(pianoKey != null){
-			//						pianoKey.Play();
-			//					}
+						if(Input.anyKey){
+							string pressedKey = Input.inputString;
+//							if(Input.GetKeyDown(".")){
+//								keyboardToPiano["."].loop = true;
+//								keyboardToPiano["."].Play();
+//							}
+//							if(Input.GetKeyUp(".")){
+//								keyboardToPiano["."].Stop();
+//							}
+							try{
+								AudioSource pianoKey = keyboardToPiano[pressedKey];
+								if(pianoKey != null){
+									pianoKey.Play();
+								}
 
 			
-			//				}catch(System.Exception e){
-			//					//do nothing
-			//				}
-			//			}
+							}catch(System.Exception e){
+								//do nothing
+							}
+						}
 		}
 	}
 }
